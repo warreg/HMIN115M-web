81d03b5a030c97358b0d2d4c8d75360045a73dda
Marone <45037484+JSedar@users.noreply.github.com>
2019-12-04 14:16:05 +0100

    Update connexion.js

--- PROJET/JS/connexion.js
+++ PROJET/JS/connexion.js
@@ -1,16 +1,7 @@
 window.addEventListener("DOMContentLoaded", function() {
 
 
-    //Envoi des infos de connexion
-    var form = document.querySelector("#form-ajout");
-    form.addEventListener("submit", function(e) {
-        e.preventDefault();
-        var new_recette = new FormData(form);
-        //var input = document.querySelectorAll("input");
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST","../PHP/wsConnexion.php");
-        xhr.send(new_recette);
-    });
+    
 
 
 });
